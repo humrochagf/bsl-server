6034b64dd0d6264f217f92818bf637fa062138b3
Humberto Rocha <humrochagf@gmail.com>
2014-08-18 19:23:51 -0300

    view de index

--- bsl/core/views.py
+++ bsl/core/views.py
@@ -1,5 +1,26 @@
+from base64 import b64encode
+from hashlib import sha1
+from io import BytesIO
+from os import urandom
+
+import qrcode
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("Hello World!")
+    # Generate the nonce
+    # TODO: Work on the entropy and size of the nonce
+    token = sha1(urandom(128)).hexdigest()
+
+    # Create a temporary placeholder to the image
+    stream = BytesIO()
+
+    # Generate the qrcode and save to the stream
+    qrcode.make(token).save(stream)
+
+    # Get the image bytes, then encode to base64
+    data = b64encode(stream.getvalue())
+
+    context = {'token': token, 'qrcode': data}
+
+    return render(request, 'core/index.html', context)
