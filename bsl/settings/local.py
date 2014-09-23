d4a0fd2e820dcdfaf9e3a470e181c2e846d88090
Humberto Rocha <humrochagf@gmail.com>
2014-09-23 12:05:46 -0300

    migrando settings para seus respectivos arquivos

--- bsl/settings/local.py
+++ bsl/settings/local.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
+import dj_database_url
+
+from .base import *
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+SECRET_KEY = '&mc9cnu9q9io^hk^a%5!##+u1c#yp*50osoxca@%xc+h7$@!f8'
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
+}
--- bsl/settings/production.py
+++ bsl/settings/production.py
@@ -1,1 +1,24 @@
 # -*- coding: utf-8 -*-
+import dj_database_url
+
+from . import get_env_variable
+from .base import *
+
+
+SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+CSRF_COOKIE_SECURE = True
+
+SESSION_COOKIE_SECURE = True
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
