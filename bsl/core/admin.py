f88b3673a4e1716e3b40e26cd0c3d442b6b9a730
Humberto Rocha <humrochagf@gmail.com>
2014-09-26 10:43:55 -0300

    fix alteração e criação de usuário não funcionando

--- bsl/core/admin.py
+++ bsl/core/admin.py
@@ -1,6 +1,33 @@
+# -*- coding: utf-8 -*-
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+
+admin.site.register(User, CustomUserAdmin)
